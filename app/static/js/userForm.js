a15bcf299e7de95bed30c5a5447b04c37baca4d0
David Richards <davidr@mobbu.com>
2015-12-05 15:51:49 +0000

    Add validation error msg for name and email and filler text for particpant list

--- app/static/js/userForm.js
+++ app/static/js/userForm.js
@@ -4,19 +4,28 @@ var userForm = (function($, userList) {
     var nameField;
     var emailField;
     var usersList;
+    var errorMessage;
+    var errorText;
     
     var init = function() {
         addButton = $('.btn-add-user');
         nameField = $('input.user-name');
         emailField = $('input.user-email');
         usersList = $('.userlist');
+        errorMessage = $('.error-popup');
+        errorText = $('.error-popup .error-text')
         addButton.click(onAddClicked);
     };
     
     var onAddClicked = function(e) {
+        errorMessage.hide();
         var userName = nameField.val();
         var userEmail = emailField.val();
-        if (isValidName(userName) && isValidEmail(userEmail)) {
+        if (!isValidName(userName)) {
+            showError('Sorry, that name is too short!');
+        } else if (!isValidEmail(userEmail)) {
+            showError('Sorry, that\'s not a valid email! If you don\'t want to add one, you can leave it blank');
+        } else {
             RequestHandler.postUser({'name':userName, 'email':userEmail}, onAddSuccess, onAddFailed);
         }
     };
@@ -26,7 +35,7 @@ var userForm = (function($, userList) {
     };
     
     var isValidEmail = function(email) {
-        return email.length > 3 && email.indexOf('@') > -1;
+        return (email === '' || (email.length > 3 && email.indexOf('@') > -1));
     };
     
     var onAddSuccess = function(data, textStatus, jqXHR) {
@@ -39,6 +48,11 @@ var userForm = (function($, userList) {
     
     var onAddFailed = function(jqXHR, textStatus, error) {
         console.error(jqXHR);
+    };
+    
+    var showError = function(message) {
+        errorText.text(message);
+        errorMessage.show();
     };
     
     return {
