de9faa587466ce118f38400c9f6babd6687e9d6e
David Richards <davidr@mobbu.com>
2015-12-22 11:28:00 +0000

    added cookie generation for user based persistence

--- app/views.py
+++ app/views.py
@@ -1,15 +1,28 @@
 from app import app, persistence, match
-from flask import render_template, url_for, request, jsonify, Response
+from flask import render_template, make_response, url_for, request, jsonify, Response
 import json
+import uuid
 
 def update_matches(userList):
     # update match list and store
     matches = match.get_match_for_users(userList)
     persistence.add_matches(matches)
-
+    
+def generate_cookie():
+    cookie_id = uuid.uuid4()
+    #persistence.add_session(cookie_id)
+    return cookie_id
+    
 @app.route('/')
 def root():
-    return render_template('index.html', users=persistence.get_users())
+    user_cookie = request.cookies.get('santaselector')
+    if not user_cookie:
+        user_cookie = generate_cookie()
+        response = make_response(render_template('index.html', users=persistence.get_users()))
+        response.set_cookie('santaselector', value=bytes(user_cookie))
+        return response
+    else:
+        return render_template('index.html', users=persistence.get_users())
 
 @app.route('/user', methods=['POST'])
 def post():
